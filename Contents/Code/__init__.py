a086a7f41a3c4af7eeb39a2ca3cf69168e937636
Cigaras <Cigaras@gmail.com>
2013-10-11 11:39:55 +0300

    Initial realease

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2013 Valdas Vaitiekaitis, a.k.a. Cigaras. All rights reserved.
+
+PREFIX = '/video/iptv'
+TITLE = 'IPTV'
+
+def Start():
+    ObjectContainer.title1 = TITLE
+
+    # Default icon for directories (groups)
+    DirectoryObject.thumb = R('icon-default.png')
+
+    # I don't know what it is
+    HTTP.CacheTime = CACHE_1HOUR
+
+@handler(PREFIX, TITLE)
+def MainMenu():
+    items_list = []
+    groups_list = []
+    playlist = Resource.Load('playlist.m3u', binary = True)
+    if playlist <> None:
+        lines = playlist.splitlines()
+        for i in range(len(lines) - 1):
+            line = lines[i].strip()
+            if line.startswith('#EXTINF'):
+                url = lines[i + 1].strip()
+                title = line[line.rfind(',') + 1:len(line)].strip()
+                thumb = GetParm(line, 'tvg-logo')
+                group = GetParm(line, 'group-title')
+                items_list.append({'url': url, 'title': title, 'thumb': thumb, 'group': group})
+                if group <> '' and not group in groups_list:
+                    groups_list.append(group)
+        items_list.sort(key = lambda dict: dict['title'].lower())
+        groups_list.sort(key = lambda str: str.lower())
+        groups_list.insert(0, 'All')
+        groups_list.append('Uncategorized')
+
+    if len(items_list) < 1:
+        return ObjectContainer(header="No items found", message="Make sure file playlist.m3u is present and not empty in plugins Resources folder")
+
+    oc = ObjectContainer()
+
+    for group in groups_list:
+        oc.add(DirectoryObject(
+            key = Callback(ItemsMenu, items_list = items_list, group = group),
+            title = group
+        ))
+
+    return oc
+
+@route(PREFIX + '/itemsmenu', items_list = list)
+def ItemsMenu(items_list, group):
+    oc = ObjectContainer(title1 = group)
+    for item in items_list:
+        if item['group'] == group or group == 'All' or (item['group'] == '' and group == 'Uncategorized'):
+            url = item['url']
+            title = item['title']
+            thumb = item['thumb']
+            oc.add(VideoClipObject(
+                key = Callback(Lookup, url = url, title = title, thumb = thumb),
+                rating_key = url,
+                title = title,
+                thumb = thumb,
+                items = [
+                    MediaObject(
+                        #container = Container.MP4,     # MP4, MKV, MOV, AVI
+                        #video_codec = VideoCodec.H264, # H264
+                        #audio_codec = AudioCodec.AAC,  # ACC, MP3
+                        #audio_channels = 2,            # 2, 6
+                        #parts = [PartObject(key = HTTPLiveStreamURL(url))],
+                        parts = [PartObject(key = url)],
+                        optimized_for_streaming = True
+                    )
+                ]
+            ))
+    return oc
+
+@route(PREFIX + '/lookup')
+def Lookup(url, title, thumb):
+    oc = ObjectContainer()
+    oc.add(VideoClipObject(
+        key = Callback(Lookup, url = url, title = title, thumb = thumb),
+        rating_key = url,
+        title = title,
+        thumb = thumb,
+        items = [
+            MediaObject(
+                #container = Container.MP4,
+                #video_codec = VideoCodec.H264,
+                #audio_codec = AudioCodec.AAC,
+                #audio_channels = 2,
+                #parts = [PartObject(key = HTTPLiveStreamURL(url))],
+                parts = [PartObject(key = url)],
+                optimized_for_streaming = True
+            )
+        ]
+    ))
+    return oc
+
+def GetParm(text, parm):
+    x = text.find(parm)
+    if x > -1:
+        y = text.find('"', x + len(parm)) + 1
+        z = text.find('"', y)
+        return text[y:z].strip()
+    else:
+        return ''
---
+++ README.md
@@ -0,0 +1,9 @@
+Plex Media Server plugin to play IPTV streams from M3U playlist.
+
+Playlist must be generated using M3U standarts (http://en.wikipedia.org/wiki/M3U), stored in Contents\Resources forlder and named playlist.m3u, there are some additional attributes supported:
+* tvg-logo
+* group-title
+
+Included sample playlist will only work for me so you'll have to make your own, I don't have any public streams to give as an example.
+
+Copyright (c) 2013 Valdas Vaitiekaitis, a.k.a. Cigaras. All rights reserved.
