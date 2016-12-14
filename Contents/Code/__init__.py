60f535ecf61959446e2e0043e770ce55ce402c78
Cigaras <Cigaras@gmail.com>
2016-12-14 15:24:30 +0200

    Version 1.2.7
    * When no groups/categories are defined all the streams are listed
    directly in main menu
    * New playlist attributes art and group-art to define personal
    backgrounds
    * GZIP compression support for online guides (no other compressions or
    local files for the moment)

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -10,12 +10,15 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-# Version 1.2.6
+# Version 1.2.7
 
-from datetime import datetime, timedelta # https://docs.python.org/2/library/datetime.html
-import xml.etree.ElementTree # https://docs.python.org/2/library/xml.etree.elementtree.html
+from urllib import urlopen
+from io import BytesIO
+from gzip import GzipFile
+import xml.etree.ElementTree
 import time
 import calendar
+from datetime import datetime, timedelta
 
 TITLE = 'IPTV'
 PREFIX = '/video/iptv'
@@ -35,83 +38,34 @@ def MainMenu():
     LoadPlaylist()
     LoadGuide()
     groups_list = GROUPS.values()
-    if Prefs['sort_groups']:
-        groups_list.sort(key = lambda dict: dict['title'].lower())
-    else:
-        groups_list.sort(key = lambda dict: dict['order'])
-    oc = ObjectContainer()
-    oc.add(DirectoryObject(key = Callback(ListItems, group = unicode(L('All'))), title = unicode(L('All'))))
+    use_groups = False
     for group in groups_list:
         if group['title'] not in [unicode(L('All')), unicode(L('No Category'))]:
-            thumb = GetThumb(group['thumb'], default = 'icon-folder.png')
-            oc.add(DirectoryObject(
-                key = Callback(ListItems, group = group['title']),
-                title = group['title'],
-                thumb = thumb
-            ))
-    if unicode(L('No Category')) in GROUPS.keys():
-        oc.add(DirectoryObject(key = Callback(ListItems, group = unicode(L('No Category'))), title = unicode(L('No Category'))))
-    oc.add(PrefsObject(title = unicode(L('Preferences')), thumb = R('icon-prefs.png')))
-    return oc
-
-def LoadPlaylist():
-    if Prefs['playlist'].startswith('http://') or Prefs['playlist'].startswith('https://'):
-        playlist = HTTP.Request(Prefs['playlist']).content
+            use_groups = True
+            break
+    if use_groups:
+        if Prefs['sort_groups']:
+            groups_list.sort(key = lambda dict: dict['title'].lower())
+        else:
+            groups_list.sort(key = lambda dict: dict['order'])
+        oc = ObjectContainer()
+        oc.add(DirectoryObject(key = Callback(ListItems, group = unicode(L('All'))), title = unicode(L('All'))))
+        for group in groups_list:
+            if group['title'] not in [unicode(L('All')), unicode(L('No Category'))]:
+                thumb = GetThumb(group['thumb'], default = 'icon-folder.png')
+                alt = GetThumb(group['art'], default = 'art-default.png')
+                oc.add(DirectoryObject(
+                    key = Callback(ListItems, group = group['title']),
+                    title = group['title'],
+                    thumb = thumb,
+                    art = art
+                ))
+        if unicode(L('No Category')) in GROUPS.keys():
+            oc.add(DirectoryObject(key = Callback(ListItems, group = unicode(L('No Category'))), title = unicode(L('No Category'))))
+        oc.add(PrefsObject(title = unicode(L('Preferences')), thumb = R('icon-prefs.png')))
+        return oc
     else:
-        playlist = Resource.Load(Prefs['playlist'], binary = True)
-    if playlist != None:
-        lines = playlist.splitlines()
-        groups_count = 0
-        streams_count = 0
-        for i in range(len(lines) - 1):
-            line = lines[i].strip()
-            if line.startswith('#EXTINF'):
-                url = lines[i + 1].strip()
-                if url != '' and not url.startswith('#'):
-                    title = line[line.rfind(',') + 1:len(line)].strip()
-                    id = GetAttribute(line, 'tvg-id')
-                    name = GetAttribute(line, 'tvg-name')
-                    thumb = GetAttribute(line, 'tvg-logo')
-                    if thumb == '':
-                        thumb = GetAttribute(line, 'logo')
-                    streams_count = streams_count + 1
-                    stream = {'url': url, 'title': title, 'id': id, 'name': name, 'thumb': thumb, 'order': streams_count}
-                    if not STREAMS:
-                        STREAMS.setdefault(unicode(L('All')), {})[streams_count] = stream
-                    if STREAMS:
-                        if not any(item['url'] == stream['url'] for item in STREAMS[unicode(L('All'))].values()):
-                            STREAMS.setdefault(unicode(L('All')), {})[streams_count] = stream
-                        group_title = GetAttribute(line, 'group-title', default = unicode(L('No Category')))
-                        if group_title not in GROUPS.keys():
-                            group_thumb = GetAttribute(line, 'group-logo')
-                            groups_count = groups_count + 1
-                            group = {'title': group_title, 'thumb': group_thumb, 'order': groups_count}
-                            GROUPS[group_title] = group
-                        if group_title in STREAMS.keys():
-                            if not any(item['url'] == stream['url'] for item in STREAMS[group_title].values()):
-                                STREAMS.setdefault(group_title, {})[streams_count] = stream
-                        else:
-                            STREAMS.setdefault(group_title, {})[streams_count] = stream
-                    i = i + 1 # skip the url line for the next cycle
-    return None
-
-def LoadGuide():
-    if Prefs['xmltv'].startswith('http://') or Prefs['xmltv'].startswith('https://'):
-        xmltv = HTTP.Request(Prefs['xmltv']).content
-    else:
-        xmltv = Resource.Load(Prefs['xmltv'], binary = True)
-    if xmltv != None:
-        root = xml.etree.ElementTree.fromstring(xmltv)
-        count = 0
-        for programme in root.findall("./programme"):
-            channel = programme.get('channel')
-            start = datetime_from_utc_to_local(programme.get('start'))
-            stop = datetime_from_utc_to_local(programme.get('stop'))
-            title = programme.find('title').text
-            count = count + 1
-            item = {'start': start, 'stop': stop, 'title': title, 'order': count}
-            GUIDE.setdefault(channel, {})[count] = item
-    return None
+        return ListItems(unicode(L('All')))
 
 @route(PREFIX + '/groups/{group}', page = int)
 def ListItems(group, page = 1):
@@ -138,22 +92,6 @@ def ListItems(group, page = 1):
         # Some clients fail if summary is left empty
         if not summary:
             summary = item['title']
-        #oc.add(VideoClipObject(
-        #    url = item['url'],
-        #    title = item['title'],
-        #    thumb = GetThumb(item['thumb']),
-        #    summary = summary,
-        #    items = [
-        #        MediaObject(
-        #            parts = [
-        #                PartObject(
-        #                    key = GetVideoURL(url = item['url']),
-        #                )
-        #            ],
-        #            optimized_for_streaming = True
-        #        )
-        #    ]
-        #))
         # Simply adding VideoClipObject does not work on some clients (like LG SmartTV),
         # so there is an endless recursion - function CreateVideoClipObject calling itself -
         # and I have no idea why and how it works...
@@ -161,6 +99,7 @@ def ListItems(group, page = 1):
             url = item['url'],
             title = item['title'],
             thumb = item['thumb'],
+            art = item['art'],
             summary = summary
         ))
     if len(items_list) > page * items_per_page:
@@ -169,6 +108,113 @@ def ListItems(group, page = 1):
         return ObjectContainer(header = "Empty", message = "There are no more items available") # this should not ever happen
     else:
         return oc
+
+@route(PREFIX + '/createvideoclipobject')
+def CreateVideoClipObject(url, title, thumb, art, summary = None, container = False):
+    vco = VideoClipObject(
+        key = Callback(CreateVideoClipObject, url = url, title = title, thumb = thumb, art = art, summary = summary, container = True),
+        rating_key = title,
+        url = url,
+        title = title,
+        thumb = GetThumb(thumb, default = 'icon-default.png'),
+        art = GetThumb(art, default = 'art-default.jpg'),
+        summary = summary,
+        items = [
+            MediaObject(
+                parts = [
+                    PartObject(
+                        key = GetVideoURL(url = url),
+                        # iOS client shows permanent loading screen in the foreground if no duration is provided (https://forums.plex.tv/discussion/comment/1293745/#Comment_1293745),
+                        # smarter clients understand that it is a live stream and ignore this property
+                        duration = 86400000 # 24 hours
+                    )
+                ],
+                optimized_for_streaming = Prefs['optimized_for_streaming'] # https://forums.plex.tv/discussion/comment/828497/#Comment_828497
+            )
+        ]
+    )
+    if container:
+        return ObjectContainer(objects = [vco])
+    else:
+        return vco
+    return vco
+
+def LoadPlaylist():
+    if Prefs['playlist'].startswith('http://') or Prefs['playlist'].startswith('https://'):
+        playlist = HTTP.Request(Prefs['playlist']).content
+    else:
+        playlist = Resource.Load(Prefs['playlist'], binary = True)
+    if playlist != None:
+        lines = playlist.splitlines()
+        groups_count = 0
+        streams_count = 0
+        for i in range(len(lines) - 1):
+            line = lines[i].strip()
+            if line.startswith('#EXTINF'):
+                url = lines[i + 1].strip()
+                if url != '' and not url.startswith('#'):
+                    title = line[line.rfind(',') + 1:len(line)].strip()
+                    id = GetAttribute(line, 'tvg-id')
+                    name = GetAttribute(line, 'tvg-name')
+                    thumb = GetAttribute(line, 'tvg-logo')
+                    if thumb == '':
+                        thumb = GetAttribute(line, 'logo')
+                    art = GetAttribute(line, 'art')
+                    streams_count = streams_count + 1
+                    stream = {'url': url, 'title': title, 'id': id, 'name': name, 'thumb': thumb, 'art': art, 'order': streams_count}
+                    if not STREAMS:
+                        STREAMS.setdefault(unicode(L('All')), {})[streams_count] = stream
+                    if STREAMS:
+                        if not any(item['url'] == stream['url'] for item in STREAMS[unicode(L('All'))].values()):
+                            STREAMS.setdefault(unicode(L('All')), {})[streams_count] = stream
+                        group_title = GetAttribute(line, 'group-title', default = unicode(L('No Category')))
+                        if group_title not in GROUPS.keys():
+                            group_thumb = GetAttribute(line, 'group-logo')
+                            group_art = GetAttribute(line, 'group-art')
+                            groups_count = groups_count + 1
+                            group = {'title': group_title, 'thumb': group_thumb, 'art': group_art, 'order': groups_count}
+                            GROUPS[group_title] = group
+                        if group_title in STREAMS.keys():
+                            if not any(item['url'] == stream['url'] for item in STREAMS[group_title].values()):
+                                STREAMS.setdefault(group_title, {})[streams_count] = stream
+                        else:
+                            STREAMS.setdefault(group_title, {})[streams_count] = stream
+                    i = i + 1 # skip the url line for the next cycle
+    return None
+
+def LoadGuide():
+    if Prefs['xmltv'].startswith('http://') or Prefs['xmltv'].startswith('https://'):
+        # Plex can't handle compressed files, using standart Python methods instead
+        if Prefs['xmltv'].endswith('.gz') or Prefs['xmltv'].endswith('.gz?raw=1'):
+            f = BytesIO(urlopen(Prefs['xmltv']).read())
+            try:
+                g = GzipFile(fileobj = f)
+                xmltv = g.read()
+            except:
+                Log.Error('Provided file %s is not a valid GZIP file' % Prefs['xmltv'])
+                xmltv = None
+        else:
+            xmltv = HTTP.Request(Prefs['xmltv']).content
+    else:
+        # Local compressed files are not supported at the moment
+        xmltv = Resource.Load(Prefs['xmltv'], binary = True)
+    if xmltv != None:
+        try:
+            root = xml.etree.ElementTree.fromstring(xmltv)
+        except:
+            Log.Error('Provided file %s is not a valid XML file' % Prefs['xmltv'])
+            root = None
+        if root != None:
+            count = 0
+            for programme in root.findall("./programme"):
+                channel = programme.get('channel')
+                start = datetime_from_utc_to_local(programme.get('start'))
+                stop = datetime_from_utc_to_local(programme.get('stop'))
+                title = programme.find('title').text
+                count = count + 1
+                item = {'start': start, 'stop': stop, 'title': title, 'order': count}
+                GUIDE.setdefault(channel, {})[count] = item
+    return None
 
 def GetAttribute(text, attribute, delimiter1 = '="', delimiter2 = '"', default = ''):
     x = text.find(attribute)
@@ -215,35 +261,6 @@ def GetGuide(channel):
             if item['start'] <= current_time + timedelta(hours = guide_hours) and item['stop'] > current_time:
                 summary = summary + '\n' + item['start'].strftime('%H:%M') + ' ' + item['title']
     return summary
-
-@route(PREFIX + '/createvideoclipobject')
-def CreateVideoClipObject(url, title, thumb, summary = None, container = False):
-    vco = VideoClipObject(
-        key = Callback(CreateVideoClipObject, url = url, title = title, thumb = thumb, summary = summary, container = True),
-        rating_key = title,
-        url = url,
-        title = title,
-        thumb = GetThumb(thumb),
-        summary = summary,
-        items = [
-            MediaObject(
-                parts = [
-                    PartObject(
-                        key = GetVideoURL(url = url),
-                        # iOS client shows permanent loading screen in the foreground if no duration is provided (https://forums.plex.tv/discussion/comment/1293745/#Comment_1293745),
-                        # smarter clients understand that it is a live stream and ignore this property
-                        duration = 86400000 # 24 hours
-                    )
-                ],
-                optimized_for_streaming = Prefs['optimized_for_streaming'] # https://forums.plex.tv/discussion/comment/828497/#Comment_828497
-            )
-        ]
-    )
-    if container:
-        return ObjectContainer(objects = [vco])
-    else:
-        return vco
-    return vco
 
 def GetVideoURL(url, live = True):
     if url.startswith('rtmp') and Prefs['rtmp']:
